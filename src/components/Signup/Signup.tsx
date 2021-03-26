c377509531a7cfad6d333f7de76810e2f124c074
Ayush Singh <Ayush.Singh@einfochips.com>
2021-03-26 18:49:49 +0530

    (code update for routing on signup)

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./Signup.css";
 import axios from "axios";
 import UserDetails from "../UserDetails";
+import { Route, Link, Redirect, BrowserRouter } from "react-router-dom";
 
 class Signup extends Component {
   state = {
@@ -16,6 +17,7 @@ class Signup extends Component {
     confirmPassword: "",
     confirmPasswordError: "",
     emailAlreadyExist: false,
+    registerSucess: false,
   };
 
   data = {
@@ -164,120 +166,134 @@ class Signup extends Component {
       console.log(userProfile);
       if (userProfile.userEmail === this.state.userEmail) {
         this.state.emailAlreadyExist = true;
+      } else {
+        this.state.emailAlreadyExist = false;
       }
     });
     if (valid && passwordMatch && this.state.emailAlreadyExist) {
       alert("email already exist");
     } else if (valid && passwordMatch && !this.state.emailAlreadyExist) {
       this.postDataHandler();
+      this.setState({ registerSucess: true });
     }
   };
   render() {
+    let redirect;
+    if (this.state.registerSucess === true) {
+      redirect = <Redirect to="/" />;
+    }
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-10 col-xl-9 mx-auto">
-            <div className="card card-signin flex-row my-5">
-              <div className="card-body">
-                <h5 className="card-title text-center">Register</h5>
-                {/* <form className="form-signin" onSubmit={this.handleSubmit}> */}
-                <form onSubmit={this.handleSubmit} className="form-signin">
-                  <div className="form-label-group">
-                    <input
-                      type="text"
-                      id="Username"
-                      name="Username"
-                      className="form-control"
-                      placeholder="Username"
-                      onChange={(event) =>
-                        this.setState({ userName: event.target.value })
-                      }
-                    />
-                    <label htmlFor="Username" className="text-muted">
-                      Username
-                    </label>
-                    <div className="inputError">{this.state.userNameError}</div>
-                  </div>
-
-                  <div className="form-label-group">
-                    <input
-                      type="text"
-                      name="email"
-                      id="email"
-                      placeholder="Email"
-                      className="form-control"
-                      value={this.state.userEmail}
-                      onChange={(event) =>
-                        this.setState({ userEmail: event.target.value })
-                      }
-                    />
-                    <label htmlFor="email" className="text-muted">
-                      email
-                    </label>
-                    <div className="inputError">{this.state.emailError}</div>
-                  </div>
-
-                  <div className="form-label-group">
-                    <input
-                      type="text"
-                      id="inputPhoneNumber"
-                      name="inputPhoneNumber"
-                      className="form-control"
-                      placeholder="Phone Number"
-                      onChange={(event) =>
-                        this.setState({ phoneNumber: event.target.value })
-                      }
-                    />
-                    <label htmlFor="inputPhoneNumber" className="text-muted">
-                      PhoneNumber
-                    </label>
-                  </div>
-                  <hr />
-
-                  <div className="form-label-group">
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      className="form-control"
-                      placeholder="Password"
-                      onChange={(event) =>
-                        this.setState({ password: event.target.value })
-                      }
-                    />
-                    <label htmlFor="password" className="text-muted">
-                      password
-                    </label>
-                    <div className="inputError">{this.state.passwordError}</div>
-                  </div>
-
-                  <div className="form-label-group">
-                    <input
-                      type="password"
-                      id="ConfirmPassword"
-                      name="ConfirmPassword"
-                      className="form-control"
-                      placeholder="Password"
-                      onChange={(event) =>
-                        this.setState({ confirmPassword: event.target.value })
-                      }
-                    />
-                    <label htmlFor="ConfirmPassword" className="text-muted">
-                      ConfirmPassword
-                    </label>
-                    <div className="inputError">
-                      {this.state.confirmPasswordError}
+      <div>
+        {redirect}
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-10 col-xl-9 mx-auto">
+              <div className="card card-signin flex-row my-5">
+                <div className="card-body">
+                  <h5 className="card-title text-center">Register</h5>
+                  {/* <form className="form-signin" onSubmit={this.handleSubmit}> */}
+                  <form onSubmit={this.handleSubmit} className="form-signin">
+                    <div className="form-label-group">
+                      <input
+                        type="text"
+                        id="Username"
+                        name="Username"
+                        className="form-control"
+                        placeholder="Username"
+                        onChange={(event) =>
+                          this.setState({ userName: event.target.value })
+                        }
+                      />
+                      <label htmlFor="Username" className="text-muted">
+                        Username
+                      </label>
+                      <div className="inputError">
+                        {this.state.userNameError}
+                      </div>
                     </div>
-                  </div>
-                  {/* <input type="submit" name="" value="Login" /> */}
-                  <button
-                    className="btn btn-lg btn-primary btn-block text-uppercase"
-                    type="submit"
-                  >
-                    {" "}
-                    Register{" "}
-                  </button>
-                </form>
+
+                    <div className="form-label-group">
+                      <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        className="form-control"
+                        value={this.state.userEmail}
+                        onChange={(event) =>
+                          this.setState({ userEmail: event.target.value })
+                        }
+                      />
+                      <label htmlFor="email" className="text-muted">
+                        email
+                      </label>
+                      <div className="inputError">{this.state.emailError}</div>
+                    </div>
+
+                    <div className="form-label-group">
+                      <input
+                        type="text"
+                        id="inputPhoneNumber"
+                        name="inputPhoneNumber"
+                        className="form-control"
+                        placeholder="Phone Number"
+                        onChange={(event) =>
+                          this.setState({ phoneNumber: event.target.value })
+                        }
+                      />
+                      <label htmlFor="inputPhoneNumber" className="text-muted">
+                        PhoneNumber
+                      </label>
+                    </div>
+                    <hr />
+
+                    <div className="form-label-group">
+                      <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="form-control"
+                        placeholder="Password"
+                        onChange={(event) =>
+                          this.setState({ password: event.target.value })
+                        }
+                      />
+                      <label htmlFor="password" className="text-muted">
+                        password
+                      </label>
+                      <div className="inputError">
+                        {this.state.passwordError}
+                      </div>
+                    </div>
+
+                    <div className="form-label-group">
+                      <input
+                        type="password"
+                        id="ConfirmPassword"
+                        name="ConfirmPassword"
+                        className="form-control"
+                        placeholder="Password"
+                        onChange={(event) =>
+                          this.setState({ confirmPassword: event.target.value })
+                        }
+                      />
+                      <label htmlFor="ConfirmPassword" className="text-muted">
+                        ConfirmPassword
+                      </label>
+                      <div className="inputError">
+                        {this.state.confirmPasswordError}
+                      </div>
+                    </div>
+                    {/* <input type="submit" name="" value="Login" /> */}
+                    <button
+                      className="btn btn-lg btn-primary btn-block text-uppercase"
+                      type="submit"
+                    >
+                      {" "}
+                      Register{" "}
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
