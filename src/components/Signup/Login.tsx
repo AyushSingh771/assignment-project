ea401b6f4459730d6cd133d39420d74972e8d89d
ayush7271 <ayush.singh@einfochips.com>
2021-06-15 01:43:57 +0530

    hello there 03src/components/Signup/Login.tsx

--- src/components/Signup/Login.tsx
+++ src/components/Signup/Login.tsx
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
 import "./Login.css";
-import axios from 'axios';
+import axios from "axios";
 // import { Link } from "react-router-dom";
-import { Route, Link, Switch } from 'react-router-dom';
-import Signup from '../Signup/Signup'
-
+import { Route, Link, Switch } from "react-router-dom";
+import Signup from "../Signup/Signup";
+// hello there i am in login
 class Login extends Component {
   state = {
     userEmail: "",
     password: "",
     emailError: "",
     passwordError: "",
-     loggedIn: "false"
+    loggedIn: "false",
   };
- data = {
-   user: []
- }
-  componentDidMount () {
-        // console.log(this.props);
-        axios.get( 'http://localhost:8000/profile' )
-            .then( response => {
-                 this.data.user = response.data; 
-            } )
-            .catch(error => {
-                console.log(error);
-            });
-    }
+  data = {
+    user: [],
+  };
+  componentDidMount() {
+    // console.log(this.props);
+    axios
+      .get("http://localhost:8000/profile")
+      .then((response) => {
+        this.data.user = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   isValid() {
     let emailDiscription = "Please enter correct email";
@@ -70,75 +71,74 @@ class Login extends Component {
 
     return true;
   }
-  isLoggedIn(){
-      this.data.user.map((userProfile: any) => {
-                  console.log(userProfile);
-                   if(userProfile.userEmail === this.state.userEmail && userProfile.password === this.state.password) {
-                     this.setState({loggedIn: true});
-                     alert("loggedIn successful")
-                   }
-                   else{
-                    alert("invalid email or password");
-                   }
-                 });
+  isLoggedIn() {
+    this.data.user.map((userProfile: any) => {
+      console.log(userProfile);
+      if (
+        userProfile.userEmail === this.state.userEmail &&
+        userProfile.password === this.state.password
+      ) {
+        this.setState({ loggedIn: true });
+        alert("loggedIn successful");
+      } else {
+        alert("invalid email or password");
+      }
+    });
   }
   handleSubmit = (event: any) => {
     event.preventDefault();
     this.isLoggedIn();
-    this.setState({ isSubmit: "true"});
+    this.setState({ isSubmit: "true" });
     const valid = this.isValid();
     if (valid) {
       console.log(this.state);
     }
-
   };
   render() {
     return (
       <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="card">
-              
-              <form onSubmit={this.handleSubmit} className="box">
-                <h1>Login</h1>
-                <p className="text-muted">
-                  {" "}
-                  Please enter your login and password!
-                </p>
-                <input
-                  type="text"
-                  name="email"
-                  placeholder="Email"
-                  value={this.state.userEmail}
-                  onChange={(event) =>
-                    this.setState({ userEmail: event.target.value })
-                  }
-                />
-                <div className="inputError">{this.state.emailError}</div>
-                <input
-                  type="password"
-                  name="passwrod"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={(event) =>
-                    this.setState({ password: event.target.value })
-                  }
-                />
-                <div className="inputError">{this.state.passwordError}</div>
-                <a className="forgot text-muted" href="www.google.com">
-                  Forgot password?
-                </a>
-                <input type="submit" name="" value="Login" />
-                <Link to="/signup">Signup</Link>
-              </form>
-              
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <div className='card'>
+                <form onSubmit={this.handleSubmit} className='box'>
+                  <h1>Login</h1>
+                  <p className='text-muted'>
+                    {" "}
+                    Please enter your login and password!
+                  </p>
+                  <input
+                    type='text'
+                    name='email'
+                    placeholder='Email'
+                    value={this.state.userEmail}
+                    onChange={(event) =>
+                      this.setState({ userEmail: event.target.value })
+                    }
+                  />
+                  <div className='inputError'>{this.state.emailError}</div>
+                  <input
+                    type='password'
+                    name='passwrod'
+                    placeholder='Password'
+                    value={this.state.password}
+                    onChange={(event) =>
+                      this.setState({ password: event.target.value })
+                    }
+                  />
+                  <div className='inputError'>{this.state.passwordError}</div>
+                  <a className='forgot text-muted' href='www.google.com'>
+                    Forgot password?
+                  </a>
+                  <input type='submit' name='' value='Login' />
+                  <Link to='/signup'>Signup</Link>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      
-      <Route exact path="/signup" component={Signup}/>
+
+        <Route exact path='/signup' component={Signup} />
       </div>
     );
   }
